e4c7d9202a1aa37ea0afa63aebb612883d88de27
Pawan8787 <168840229+Pawan8787@users.noreply.github.com>
2025-12-06 18:59:31 +0000

    committomain

---
+++ basic.js/conversion.js
@@ -0,0 +1,16 @@
+let score = "abc"
+ //console.log(typeof (score))
+ let convertedscore = Number(score)
+ //console.table([convertedscore,typeof(convertedscore)])
+ //for boolean ""->false, "abc"->true
+ let isLOggedin = "true"
+
+
+
+ console.log(isLOggedin);
+ let changetoboolean= Boolean(isLOggedin)
+ console.table([changetoboolean, typeof(changetoboolean)])
+
+ //it means for any empty values it will return false otherwise true
+
+//null to undefined
