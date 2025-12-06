cd55ca218d6fa52b45a840448098152ebf104754
Pawan8787 <168840229+Pawan8787@users.noreply.github.com>
2025-12-06 19:00:41 +0000

    h

---
+++ basic.js/1.js
@@ -0,0 +1,6 @@
+"use strict";
+let accountID= 12345;
+   console.table([accountID])
+    // alert(3+3) we are using nodejs not browser 
+    // code readibility should be maintained
+        console.log("return type of accountID")
